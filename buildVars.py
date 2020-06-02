d7efb21c45372152977e5ea53ebf5cad7be6f9e6
Marlon <marlon.bsousa@gmail.com>
2020-06-02 09:02:18 -0300

    global improvements and nvda 2019.3 compatibility

--- buildVars.py
+++ buildVars.py
@@ -11,23 +11,25 @@ addon_info = {
 	# for previously unpublished addons, please follow the community guidelines at:
 	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
 	# add-on Name, internal for nvda
-	"addon_name" : "screenRapping",
+	"addon_name" : "screenWrapping",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on to be shown on installation and add-on information.
-	"addon_summary" : _("screen rapping for nvda"),
+	"addon_summary" : _("screen wrapping for nvda"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description" : _("""This addon brings the screen rapping feature from jaws to nvda."""),
+	"addon_description" : _("""This addon brings the screen wrapping feature to nvda.
+	When you press quick navigation keys such as h, b, f and others, you will be placed on the next element regardless of your current position on a webpage.
+	If elements are not found below your position, you will be placed at the first element of the requested type available from the beginning of tghe page and virseversa when you are searching upwards."""),
 	# version
-	"addon_version" : "1.4",
+	"addon_version" : "1.5",
 	# Author(s)
 	"addon_author" : u"Hamada Trichine <hamadalog25@gmail.com>",
 	# URL for the add-on documentation support
 	"addon_url" : None,
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
-	"addon_nvdaMinimumVersion" : "2017.3",
-	"addon_lastTestedNvdaVersion" : "2018.4",
+	"addon_nvdaMinimumVersion" : "2019.3",
+	"addon_lastTestedNvdaVersion" : "2019.3",
 	"addon_updateChannel" : None,
 }
 
@@ -36,7 +38,7 @@ import os.path
 
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
-pythonSources = []
+pythonSources = [os.path.join("addon", "globalPlugins", "screenWrapping", "*.py")]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
